dfd29596040faa1ec3734612f1033761264d3fc8
ahmed <ahmedlearn01@gmail.com>
2022-08-08 10:53:24 +0300

    add to cart

--- src/components/StoreItems.tsx
+++ src/components/StoreItems.tsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import formatCurrency from "../utilities/formatCurrency";
 
 interface itemProps {
@@ -8,13 +8,19 @@ interface itemProps {
     imgUrl: string
 }
  export default function({id, name, price, imgUrl}: itemProps){
-    return <Card>
+    let quantity = 0;
+    return <Card className="h-100">
         <Card.Img variant="top" height="200px" src={imgUrl} style={{objectFit: "cover"}}/>
         <Card.Body className="d=flex flex-column">
             <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
                 <span className="fs-2">{name}</span>
                 <span className="ms-2 text-muted">{formatCurrency(price)}</span>
             </Card.Title>
+            <div className="mt-auto">
+            {quantity === 0 ? (
+                <Button>Add To Cart</Button>
+                ) : (<Button>- +</Button>)}
+            </div>
         </Card.Body>
     </Card>
 } 
