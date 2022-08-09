f6a2ceed022b6793aacc7c120f305d66e195aa83
ahmed <ahmedlearn01@gmail.com>
2022-08-09 12:06:27 +0300

    cart porps

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,16 +1,24 @@
-import { createContext, ReactNode, useContext} from 'react'
+import { createContext, ReactNode, useContext, useState} from 'react'
 
 interface shopCartProps{
     children: ReactNode
 }
 
-const ShoppingCartContext = createContext({})
+interface shopCartCont {
+    getItemQ : (id: number) => number
+    incCartQ: (id: number) => void
+    decCartQ: (id: number) => void
+    removeCart: (id: number) => void
+}
+const ShoppingCartContext = createContext({} as shopCartCont)
 
 export function useShoppingCart(){
     return useContext(ShoppingCartContext)
 }
 
 export function ShoppingCartProvider({children}: shopCartProps){
+    const [cartItems, setCartItems] = useState([])
+
     return (
         <ShoppingCartContext.Provider value={{}}>
             {children}
